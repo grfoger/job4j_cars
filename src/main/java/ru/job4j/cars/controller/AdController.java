d5fb4cb740a11b96852a25b27b1501b0f9509f3f
Arhip <grfoger@gmail.com>
2022-09-08 15:26:01 +0300

    add base index page

--- src/main/java/ru/job4j/cars/controller/AdController.java
+++ src/main/java/ru/job4j/cars/controller/AdController.java
@@ -1,4 +1,19 @@
 package ru.job4j.cars.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
 public class AdController {
+
+
+    @GetMapping("/index")
+    public String index(Model model, HttpSession session) {
+        model.addAttribute("ads", List.of());
+        return "index";
+    }
 }
